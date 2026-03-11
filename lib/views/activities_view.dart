32a18d18fe02ce49259771f68a87fabab4cbf267
Renelle <213252481+Renelle-git@users.noreply.github.com>
2026-03-11 10:36:23 +0800

    Update activities

--- lib/views/activities_view.dart
+++ lib/views/activities_view.dart
@@ -5,27 +5,46 @@ class Activities extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.notifications_sharp),
-              title: Text('Notification 1'),
-              subtitle: Text('This is a notification'),
+    return SafeArea(
+      child: Center(
+        child: AlertDialog(
+          icon: Icon(Icons.info),
+          title: const Text('Feature Update', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+          content: const SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Sorry, This feature is not available yet.', textAlign: TextAlign.center,),
+                Text('We are working on it and it will be available soon.', textAlign: TextAlign.center,),
+                Text('Thank you for your understanding.', textAlign: TextAlign.center,),
+              ],
             ),
           ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.notifications_sharp),
-              title: Text('Notification 2'),
-              subtitle: Text('This is a notification'),
-            ),
-          ),
-        ],
+        ),
       ),
     );
+
+    // return Container(
+    //   padding: EdgeInsets.all(8.0),
+
+    // child: Column(
+    //   children: <Widget>[
+    //     Card(
+    //       child: ListTile(
+    //         leading: Icon(Icons.notifications_sharp),
+    //         title: Text('Notification 1'),
+    //         subtitle: Text('This is a notification'),
+    //       ),
+    //     ),
+    //     Card(
+    //       child: ListTile(
+    //         leading: Icon(Icons.notifications_sharp),
+    //         title: Text('Notification 2'),
+    //         subtitle: Text('This is a notification'),
+    //       ),
+    //     ),
+    //   ],
+    // ),
+    // );
     // return Obx(
     //   () => Column(
     //     children: [
